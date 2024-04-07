65cefa51d32fc4375958464fc87d74b6eb8c4a2b
ImeneDehimi <imenedehimi8@gmail.com>
2024-04-07 23:56:22 +0100

    imenesCommit

--- controllers/AdminControllers.js
+++ controllers/AdminControllers.js
@@ -1,16 +1,16 @@
-const User = require("../models/User");
+const Admin = require("../models/Admin");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   const { username, email, password } = req.body;
-  const existingUser = await User.findone(email);
+  const existingUser = await Admin.findone(email);
   if (existingUser) {
     return res.status(400).send("Email already in use");
   }
   try {
     const hashedPassword = await bcrypt.hash(password, 8);
-    const user = new User({ username, email, password: hashedPassword });
+    const user = new Admin({ username, email, password: hashedPassword });
     await user.save();
     res.json("registration successful");
   } catch (err) {
@@ -20,14 +20,14 @@ exports.register = async (req, res) => {
 exports.login = async(req, res)=>{
     const{email, password}=req.body;
     try{
-        const user =  await  User.findOne(email);
+        const user =  await  Admin.findOne(email);
         if(!user){
             return res.status(404).send('no user found with this email')
         }
         if(await bcrypt.compare(password, user.password)){
             return res.status(401).send("invalid password")
         }
-        const token = jwt.sign({userid:user._id},"secret",{expiresIn:"1h"});
+        const token = jwt.sign({userid:user._id},"one",{expiresIn:"1h"});
         res.json(user, token)
     }catch(err){
         return res.status(500).send(err)
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const mongoose =require("mongoose")
 require("dotenv").config();
+const userRoute = require('./routes/userroutes')
+const adminRoute = require('./routes/adminroutes')
 const app = express();
+const cors = require('cors')
 
+app.use(express.json)
+app.use("/users",userRoute)
+app.use("/admin",adminRoute)
 
-
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
 
 app.listen(process.env.PORT,()=>{
   console.log("server is runnig");
