8ff211643832baf6e95007446b1a493d7dfabc7f
berchiche willem <berwillempro@gmail.com>
2024-04-15 16:49:34 +0100

    fixes

--- controllers/AdminControllers.js
+++ controllers/AdminControllers.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   const { username, email, password } = req.body;
-  const existingUser = await Admin.findone(email);
+  const existingUser = await Admin.findOne({ email });
   if (existingUser) {
     return res.status(400).send("Email already in use");
   }
@@ -17,19 +17,19 @@ exports.register = async (req, res) => {
     console.log(err);
   }
 };
-exports.login = async(req, res)=>{
-    const{email, password}=req.body;
-    try{
-        const user =  await  Admin.findOne(email);
-        if(!user){
-            return res.status(404).send('no user found with this email')
-        }
-        if(await bcrypt.compare(password, user.password)){
-            return res.status(401).send("invalid password")
-        }
-        const token = jwt.sign({userid:user._id},"one",{expiresIn:"1h"});
-        res.json(user, token)
-    }catch(err){
-        return res.status(500).send(err)
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await Admin.findOne({ email });
+    if (!user) {
+      return res.status(404).send("no user found with this email");
     }
-}
+    if (await bcrypt.compare(password, user.password)) {
+      return res.status(401).send("invalid password");
+    }
+    const token = jwt.sign({ userid: user._id }, "one", { expiresIn: "1h" });
+    res.json(user, token);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -12,24 +12,23 @@ exports.register = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 8);
     const user = new User({ username, email, password: hashedPassword });
     await user.save();
-    res.json("registration successful");
+    res.json("Registration successful");
   } catch (err) {
     console.log(err);
   }
 };
-exports.login = async(req, res)=>{
-    const{email, password}=req.body;
-    try{
-        const user =  await  User.findOne(email);
-        if(!user){
-            return res.status(404).send('no user found with this email')
-        }
-        if(await bcrypt.compare(password, user.password)){
-            return res.status(401).send("invalid password")
-        }
-        const token = jwt.sign({userid:user._id},"secret",{expiresIn:"1h"});
-        res.json(user, token)
-    }catch(err){
-        return res.status(500).send(err)
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send("No user found with this email");
     }
-}
+    await bcrypt.compare(password, user.password);
+    const token = jwt.sign({ userid: user._id }, "secret", { expiresIn: "1h" });
+    res.status(200).json({ user, token });
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+};
